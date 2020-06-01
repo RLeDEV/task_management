fcba7dcc395bd0ad03f20650732c28c2b865a890
רז לוי <bos901@gmail.com>
2020-06-01 23:45:18 +0300

    Deleted unnecessary parts

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React from 'react';
 import Sidebar from './components/Navbar';
 import {BrowserRouter} from 'react-router-dom';
 import './App.css';
-import { AllTasks } from './components/Tasks';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Sidebar />
-        {/* <div className="tasks-view">
-          <AllTasks />
-        </div> */}
       </BrowserRouter>
     </div>
   );
