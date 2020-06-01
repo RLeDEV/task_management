6128b97e82f6a4578acb53b5f10def31dbea3b3c
Raz Levy <razlevy@admins-MacBook-Pro-2.local>
2020-06-01 22:30:59 +0300

    Added NavLinks and Routes

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import {AllTasks} from '../Tasks';
 import './index.css';
 
 export default class Navbar extends Component {
@@ -14,15 +16,41 @@ export default class Navbar extends Component {
                                         <div>Task Manager</div>
                                     </NavLink>
                                 <ul className="nav-menu">
-                                    <li className="nav-item"><i className="fas fa-chart-line"></i>Dashboard</li>
-                                    <li className="nav-item"><i className="fas fa-tasks"></i>Tasks</li>
-                                    <li className="nav-item"><i className="fas fa-plus-square"></i>Create</li>
+                                    <li className="nav-item">
+                                        <NavLink className="link" exact to="/">
+                                            <i className="fas fa-chart-line"></i>Dashboard
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="link" exact to="/tasks">
+                                            <i className="fas fa-tasks"></i>Tasks
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="link" exact to="/create">
+                                            <i className="fas fa-plus-square"></i>Create
+                                        </NavLink>
+                                    </li>
                                 </ul>
                                 <ul className="nav-manage">
                                     <li className="user-name"><div><i className="fas fa-user-circle"></i></div></li>
                                 </ul>
                             </div>
                         </div>
+                    </div>
+                    <div>
+                        {/* <Route
+                        exact path="/"
+                        render={props => <Dashboard />}
+                        /> */}
+                        <Route
+                            path="/tasks"
+                            render={props => <AllTasks />}
+                        />
+                        {/* <Route
+                        exact path="/create"
+                        render={props => <Create />}
+                        /> */}
                     </div>
                 </React.Fragment>
             </div>
