a198a2e134c5176b6c02c9b212ed44891ebc2dee
רז לוי <bos901@gmail.com>
2020-06-09 03:20:34 +0300

    ENV changes

--- server/index.js
+++ server/index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
+// Routers
+const tasks = require('./routers/tasks');
+
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/check', function (req, res) {
-    return res.send('success');
-})
+// Router usage
+app.use('/tasks', tasks);
 
-app.listen(`${process.env.BACKEND_PORT}`, () => {
+// app.get('/check', function (req, res) {
+//     return res.send('success');
+// })
+
+app.listen(process.env.BACKEND_PORT, () => {
     console.log(`Backend server is running on port ${process.env.BACKEND_PORT}`);
 })
