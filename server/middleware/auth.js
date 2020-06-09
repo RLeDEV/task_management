f96ceffc8aa02d9a14cda3b57cbb2412cbdf083d
Raz Levy <razlevy@admins-MacBook-Pro-2.local>
2020-06-09 22:04:46 +0300

    Added config for ENV

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
+dotenv.config();
+
 module.exports = (req,res,next) => {
     // Get token from header
     const token = req.header('x-auth-token');
