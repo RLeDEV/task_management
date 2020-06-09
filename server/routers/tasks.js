f8b5ed43bb7fa9a6e4a5b8790a6153ce36478237
Raz Levy <razlevy@admins-MacBook-Pro-2.local>
2020-06-09 22:04:07 +0300

    Changed Query to look better

--- server/routers/tasks.js
+++ server/routers/tasks.js
@@ -18,7 +18,10 @@ router.get('/task/:id', async (req, res) => {
 // Get all tasks by user id
 router.get('/user/:uid', async (req, res) => {
     const _uid = req.params.uid;
-    query = `SELECT taskId as id, title, description, status, added_date FROM users as u INNER JOIN havetask as ht on u.id = ht.userId INNER JOIN task as t on t.id = ht.taskId WHERE u.id = ${_uid}`;
+    query = `SELECT taskId as id, title, description, status, added_date
+             FROM users as u INNER JOIN havetask as ht on u.id = ht.userId 
+             INNER JOIN task as t on t.id = ht.taskId 
+             WHERE u.id = ${_uid}`;
     await connection.query(query, (err , results) => {
         if(err) {
             console.log(err);
