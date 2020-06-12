47632caf40a768a3323ce4ff3e369853f1ee3c4b
רז לוי <bos901@gmail.com>
2020-06-12 14:23:36 +0300

    Added cors removal

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const auth = require('./middleware/auth');
+const cors = require('cors');
 
 // Routers
 const tasks = require('./routers/tasks');
@@ -11,8 +13,9 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-// Router usage
+// // Router usage
 app.use('/tasks', tasks);
 app.use('/users', signin);
 
