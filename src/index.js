2a50f2d6e9d064fe8a4f23070e9315aaf12c6c1f
רז לוי <bos901@gmail.com>
2020-06-12 14:22:41 +0300

    Removed store from index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {createStore} from 'redux';
+// import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
+// import allReducers from './reducers/allReducers';
+
+// const store = createStore(
+//   allReducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
