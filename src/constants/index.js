5587a7304cae3c23d57612067742b5382bdfa864
רז לוי <bos901@gmail.com>
2020-06-12 14:21:07 +0300

    Added constants for redux

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const LOGIN_LOADING = 'LOGIN_LOADING';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
+export const LOGIN_LOADED = 'LOGIN_LOADED';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
