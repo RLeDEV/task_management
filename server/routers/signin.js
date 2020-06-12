04aa5e4f086f4bab804a0e91d4275a249fe05723
רז לוי <bos901@gmail.com>
2020-06-12 14:23:53 +0300

    Added authentication for already logged in user

--- server/routers/signin.js
+++ server/routers/signin.js
@@ -3,6 +3,7 @@ const connection = require('./connector');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const auth = require('../middleware/auth');
 
 dotenv.config();
 
@@ -11,10 +12,9 @@ var router = express.Router();
 // Sign-in
 router.post('/login', async (req, res) => {
     const email = req.body.email;
-    const pwd = req.body.pwd;
+    const pwd = req.body.password;
     const hashedPwd = await bcrypt.hash(pwd, 8);
-    const query = `SELECT * FROM users
-                   WHERE email = ${email} AND password = ${pwd}`
+    const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${pwd}'`
 
     try {
         await connection.query(query, (err , results) => {
@@ -22,12 +22,45 @@ router.post('/login', async (req, res) => {
                 console.log(err);
                 return res.status(401).send(JSON.stringify({ data: err }));
             }
-            const token = jwt.sign({ uid: email}, process.env.JWT_TOKEN, {'expiresIn': '2 days'});
-            localStorage.setItem('token', token);
+            const token = jwt.sign({ id: email}, process.env.JWT_TOKEN, {'expiresIn': '2 days' });
+            updateToken(token,email)
+            return res.status(200).json({
+                token,
+                user: {
+                    results
+                }
+            });
+        })
+    } catch (err) {
+        return res.status(401).json({ msg: 'An error occured while tried to logged in user' });
+    }
+})
 
-            return res.status(200).send(results, token);
+router.get('/auth', auth, async (req,res) => {
+    const email = req.user;
+    const query = `SELECT * FROM users WHERE email = '${email}'`;
+    try {
+        await connection.query(query, (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.status(401).send(JSON.stringify({ data: err }));
+            }
+            return res.status(200).json({
+                user: results
+            })
         })
     } catch (err) {
         return res.status(401).json({ msg: 'An error occured while tried to validate user in login process' });
     }
 })
+
+
+const updateToken = (token,email) => {
+    connection.query(`UPDATE users SET token = '${token}' WHERE email = '${email}'`, (err, res) => {
+        if(err){
+            console.log(err)
+        }
+    })
+}
+
+module.exports = router;
