997553cf6d2ea3e8d5bd060c64ea7916f65e8426
Raz Levy <razlevy@admins-MacBook-Pro-2.local>
2020-06-14 22:06:40 +0300

    Added alert component

---
+++ src/components/Utils/Alert/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.css';
+
+export default class Alert extends React.Component {
+    constructor(props){ 
+        super(props);
+    }
+    render() {
+        return (
+            <div className="alert">
+                <div className={this.props.type === 'warning' ? ' warning' : this.props.type === 'error' ? ' error' : ' info'}>
+                    <div className="alert-title">
+                        {this.props.type[0].toUpperCase()}:
+                    </div>
+                    <div className="alert-content">
+                        {this.props.error}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
