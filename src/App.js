963294c86ba8375de740e535d3c523fdfc4ec3ad
רז לוי <bos901@gmail.com>
2020-06-15 19:13:31 +0300

    ProtectedRouter changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Login from './components/Login';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { loadUser } from './actions/authActions';
+import Main from './components/Main';
+import {ProtectedRoute} from './components/Utils/ProtectedRoute';
 import './App.css';
 
 class App extends React.Component {
@@ -15,7 +17,10 @@ class App extends React.Component {
       <Provider store={store}>
         <div className="App">
           <BrowserRouter>
-            <Login />
+          <Switch>
+              <Route exact path='/' component={Login} />
+              <ProtectedRoute exact path='/app' component={Main}/>
+            </Switch>
           </BrowserRouter>
         </div>
       </Provider>
