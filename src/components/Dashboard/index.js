dfcb7a015bb50746656565300a875863c2e42ed0
Raz Levy <razlevy@admins-MacBook-Pro-2.local>
2020-06-16 20:45:01 +0300

    Initialized dashboard component

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default class Dashboard extends React.Component {
+    render() {
+        return (
+            <div className="dashboard-pnl">Dashboard Component</div>
+        )
+    };
+};
