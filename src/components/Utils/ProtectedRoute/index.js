3b85989af1dae7b1c95345c2c82d2fc5bf800b6b
Raz Levy <razlevy@admins-MacBook-Pro-2.local>
2020-06-16 18:01:28 +0300

    Changed conditions to use redux's state

--- src/components/Utils/ProtectedRoute/index.js
+++ src/components/Utils/ProtectedRoute/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Route,Redirect} from 'react-router-dom';
-// import { useStore } from 'react-redux';
+import { useStore } from 'react-redux';
 export const ProtectedRoute = ({ component: Component, ...rest }) => {
-    // const store = useStore();
-    // const auth = store.getState();
+    const store = useStore();
+    const auth = store.getState();
     const token = localStorage.getItem('token');
     console.log(token)
     return (
         <Route
             {...rest}
             render={props => {
-                if(token !== null) {
+                if(auth.auth.isAuthenticated !== null) {
                     return <Component {...props} />;
                 }
                 else {
