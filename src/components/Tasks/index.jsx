f8ab9d2946682ffe82ed2b27673a4b92d1d33236
רז לוי <bos901@gmail.com>
2020-06-17 20:59:37 +0300

    Minor fixes

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,67 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Task from './Task';
+import axios from 'axios';
 import './index.css';
-
-let testTasks = [
-    {
-        title: 'test1',
-        description: 'This is a test',
-        createDate: '01/06/2020',
-        estimatedDate: '03/06/2020',
-        status: 'pending'
-    },
-    {
-        title: 'test2',
-        description: 'This is the second test',
-        createDate: '01/06/2020',
-        estimatedDate: '04/06/2020',
-        status: 'pending'
-    },
-    {
-        title: 'test3',
-        description: 'This is test 3',
-        createDate: '01/06/2020',
-        estimatedDate: '04/06/2020',
-        status: 'in progress'
-    },
-    {
-        title: 'test4',
-        description: 'This is the second test',
-        createDate: '01/06/2020',
-        estimatedDate: '04/06/2020',
-        status: 'done'
-    },
-    {
-        title: 'test2',
-        description: 'This is the second test',
-        createDate: '01/06/2020',
-        estimatedDate: '04/06/2020',
-        status: 'pending'
-    },
-    {
-        title: 'test2',
-        description: 'This is the second test',
-        createDate: '01/06/2020',
-        estimatedDate: '04/06/2020',
-        status: 'pending'
-    },
-    {
-        title: 'test2',
-        description: 'This is the second test',
-        createDate: '01/06/2020',
-        estimatedDate: '04/06/2020',
-        status: 'pending'
-    }
-]
-
 class AllTasks extends Component {
     constructor(props){
         super(props);
         this.removeItem = this.removeItem.bind(this);
         this.state = {
-            tasks: testTasks,
+            tasks: [],
             uid: null
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            if(this.state.uid === null) {
+                this.setState({uid: this.props.user.user.user[0].id})
+            }
+        }, 400)
+        if(this.state.uid !== null) {
+            fetch(`http://localhost:3001/tasks/user/${this.state.uid}`)
+                .then(response => response.json())
+                .then(data => this.setState({ tasks: data.data }));
         }
     }
 
@@ -78,8 +39,12 @@ class AllTasks extends Component {
         this.setState({tasks: currTasks});
     }
 
-
     render() {
+        if(this.state.uid !== null && this.state.tasks.length < 1) {
+            fetch(`http://localhost:3001/tasks/user/${this.state.uid}`)
+            .then(response => response.json())
+            .then(data => this.setState({ tasks: data.data }));
+        }
         if(this.state.tasks !== null) {
             var tasks = this.state.tasks.map((item, i) => {
                 return (
