8a1bd97b6e67fc3203f0dddbacf971172145e122
רז לוי <bos901@gmail.com>
2020-06-17 20:58:01 +0300

    Added 404 page

---
+++ src/components/Utils/ErrPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ErrPage extends React.Component {
+    render() {
+        return (
+            <div>
+                404 PAGE NOT FOUND
+            </div>
+        )
+    }
+}
